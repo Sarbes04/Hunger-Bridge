754e3a3999011d7e18c8645af4b0ef5aff2fc47e
SARVESH SINGH <129949482+Sarbes04@users.noreply.github.com>
2025-10-17 18:37:35 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,6 +3,7 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 export const verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
+  console.log("error cp2");
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "Unauthorized: Bearer token missing" });
   }
